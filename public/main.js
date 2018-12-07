9b287dc1b8a7c531f17e53535f4c75cea8cd9fa5
Ahmet Guclu <ahmetguclu.ag@gmail.com>
2018-12-07 10:04:25 -0500

    fixed useless JS

--- public/main.js
+++ public/main.js
@@ -7,13 +7,11 @@ function scrollNav() {
     document.getElementById("navcenter").style.margin = "0px";
     document.getElementById("nav-links").style.marginTop = "0px";
     document.getElementById("nav-links").style.paddingTop = "0px";
-    document.getElemntsByClass("card-title").style.marginTop = "25vh";
   } else {
     document.getElementById("nav").style.padding = "150px 0px";
     document.getElementById("logo").style.fontSize = "55px";
     document.getElementById("navcenter").style.margin = "0px 30px";
     document.getElementById("nav-links").style.marginTop = "10px";
     document.getElementById("nav-links").style.paddingTop = "10px";
-    document.getElemntsByClass("card-title").style.marginTop = "55vh";
   }
 }
